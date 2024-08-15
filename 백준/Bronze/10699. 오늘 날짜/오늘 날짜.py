01334e45d898918f248189ab4245e2fc84ce9597
유단단 <idan9902@gmail.com>
2024-08-15 13:37:31 +0900

    Update 오늘 날짜.py

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -1,3 +1,10 @@
 import datetime
+#import=모듈(데이터)를 갖고 온다
 d=datetime.date.today()
 print(d.isoformat())
+
+#code 1,2
+import datetime
+print(str(datetime.datetime.today()[:10]))
+#or 
+print(str(datetime.datetime.now())[:10])
