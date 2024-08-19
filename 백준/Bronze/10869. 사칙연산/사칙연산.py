67bbbe6661b48e586afcd8c495f667a2bff0dfa0
유단단 <idan9902@gmail.com>
2024-08-19 19:37:02 +0900

    Update 사칙연산.py

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -4,3 +4,11 @@ print(A-B)
 print(A*B)
 print(A//B)
 print(A%B)
+
+#A + B :	A와 B를 더한다.
+#A - B :	A에서 B를 뺀다.
+#A * B :	A와 B를 곱한다.
+#A / B :	A를 B로 나눈다
+#A // B :	A를 B로 나눈 몫을 구한다.
+#A % B :	A를 B로 나눈 나머지를 구한다.
+#A ** B :	A의 B제곱
