f53b03b40c696e8971ad619f0ceda02c9da4ee3b
유단단 <idan9902@gmail.com>
2024-08-19 19:27:36 +0900

    Update A－B.py

--- 백준/Bronze/1001. A－B/A－B.py
+++ 백준/Bronze/1001. A－B/A－B.py
@@ -1,2 +1,10 @@
 A, B = input().split()
 print(int(A)-int(B))
+
+#해당 코드를
+
+A,B = map(int,input().split())
+print(A-B)
+
+#이렇게도 바꿀 수 있는데
+#map(function, iterable) 함수는 iterable의 각 요소에 function을 적용하여 새로운 iterable을 만들어준다.
